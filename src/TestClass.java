1b8f4a4f56ec30c45bb3019dff5b0a35946734fc
smoneyan <smoneyan@gmail.com>
2012-04-12 16:25:51 +0800

    Ameding Commit message

--- src/TestClass.java
+++ src/TestClass.java
@@ -7,6 +7,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("created new branch from Test-branch and it is called test-branch2 ");
+   //Staging
 	} 
 
 }
