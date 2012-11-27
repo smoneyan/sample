7c95f8456e8937ad9a166a1a4f2539b97a6cb744
smoneyan@gmail.com <smoneyan@gmail.com>
2012-11-27 14:22:57 +0800

    k-0 by subbu

--- src/TestClass.java
+++ src/TestClass.java
@@ -7,7 +7,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     public int i = 0;
-    public int j = 0;
+    public int k = 0;
 	}
 
 }
