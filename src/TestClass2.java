5b12b8fad92906ba9377d384a9e1adc3085c4543
smoneyan@gmail.com <smoneyan@gmail.com>
2012-11-27 13:20:16 +0800

    commited by second user in testclass2.java

--- src/TestClass2.java
+++ src/TestClass2.java
@@ -1,10 +1,10 @@
 
 public class TestClass2 {
   public void TestMethod1(){
-	  
+	  // Do u think there will be merge conflict now ?? 
   }
   
-  public void TestMethod2(){
-	  
+  public void TestMethod2() {
+	   // Do u think there will be merge conflict now ?? 
   }
 }
